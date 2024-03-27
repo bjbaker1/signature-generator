6add8b592b86789c6e87465c0565bdde13026565
Ben Baker <bjbaker1@gmail.com>
2024-03-27 16:12:42 -0400

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: '/project name/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/signature-generator/" : "/",
+};
